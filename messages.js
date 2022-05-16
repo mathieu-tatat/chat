059dbcc73b1612d9bc84b3cb3e4d2785c6c8a480
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-05-16 10:46:24 +0200

    rectif messages

--- messages.js
+++ messages.js
@@ -6,11 +6,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
-                message_box.innerHTML = this.responseText;
+                message_box.innerHTML = xhttp.responseText;
             }
         };
         xhttp.open("GET","messages.php" , true); // récupération de la page message
         xhttp.send()
+
+        xhttp.onerror = function(error){
+            console.error( error );}
+        
     },500) // Actualiser le chat tous les 500 ms
 
     //get the div that contains all the messages
