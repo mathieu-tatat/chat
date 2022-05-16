1bcdee0a50b7df1fd20f5bc57814617532ea059c
mathieu-tatat <mathieu.tatat@laplateforme.io>
2022-05-16 09:02:53 +0200

    modif connexion

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,7 +1,7 @@
 <?php 
     //Connexion à la base de données
   
-    $con = new PDO('mysql:host=localhost;dbname=chat;charset=utf8' , 'root', 'root');
+    $con = new PDO('mysql:host=localhost:3306;dbname=mathieu-tatat_chat;charset=utf8' , 'thechat', 'thechat');
     if(!$con){
           //si la connexion échoue , afficher :
             echo "Connexion échouée";
