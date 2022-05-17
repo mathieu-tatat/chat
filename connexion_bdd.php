88377a98c06559be201889789c1f59ea86ef060f
mathieu-tatat <91736481+mathieu-tatat@users.noreply.github.com>
2022-05-17 09:17:53 +0200

    Update connexion_bdd.php

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,7 +1,7 @@
 <?php 
     //Connexion à la base de données
   
-    $con = new PDO('mysql:host=localhost:3306;dbname=mathieu-tatat_chat;charset=utf8' , 'thechat', 'thechat');
+    $con = new PDO('mysql:host=localhost:3306;dbname=mathieu-tatat_chat;charset=utf8' , 'discords', 'discords');
     if(!$con){
           //si la connexion échoue , afficher :
             echo "Connexion échouée";
